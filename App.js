4789597850e1c0bf1f20145acd17cb0d009a3c43
Prodan Victor <victor.prodan@luminos.software>
2018-01-13 21:44:12 +0200

    push notifications implemented

--- App.js
+++ App.js
@@ -1,47 +1,46 @@
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
-import PushNotification from 'react-native-push-notification'
+import PushNotification from 'react-native-push-notification';
 
-export default class App extends Component<{}> {
+export default class App extends Component {
   componentDidMount() {
     PushNotification.configure({
       onNotification: function(notification) {
-        console.log( 'NOTIFICATION:', notification );
+        console.log('NOTIFICATION:', notification);
       }
-    })      
+    });
   }
 
   createPushNotification = () => {
-    console.log('createPushNotification called')
+    console.log('Notification function called; Notification expected to appear.');
 
     PushNotification.localNotification({
-      message: "My Notification Message"
+      message: 'Push Notification'
     });
+  };
+
+  createScheduledPushNotification = () => {
+    console.log('Scheduled Notification function called; Scheduled Notification expected to appear.');
+
     PushNotification.localNotificationSchedule({
-      message: "My Notification Message", // (required)
-      date: new Date(Date.now() + (5 * 1000))
+      message: 'Scheduled Push Notification', // (required)
+      date: new Date(Date.now() + 5 * 1000)
     });
-  }
+  };
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+        <Text style={styles.welcome}>Push Notifications Example</Text>
+        <Text style={styles.instructions}>Tap the button below to trigger a push Notification</Text>
+
+        <Text style={styles.sendText} onPress={this.createPushNotification}>
+          Send Push Notification
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text
-          onPress={this.createPushNotification}
-        >
-          New Push Notification
+
+        <Text style={styles.sendText} onPress={this.createScheduledPushNotification}>
+          Send Scheduled Push Notification
         </Text>
       </View>
     );
@@ -53,16 +52,23 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#6C7A89'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+    color: '#EEE'
   },
   instructions: {
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    color: '#EEE',
+    fontSize: 18,
+    marginBottom: 40
   },
+  sendText: {
+    color: '#013243',
+    fontSize: 18,
+    marginBottom: 40
+  }
 });
